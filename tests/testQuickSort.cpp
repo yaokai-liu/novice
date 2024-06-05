bc9e6442edbc2813b78bfb9b3dc573da7f8af782
Yaokai Liu <yaokai.liu@foxmail.com>
2024-06-05 21:14:13 +0800

    update test cases.

--- tests/testQuickSort.cpp
+++ tests/testQuickSort.cpp
@@ -2,41 +2,149 @@
 #include<cstdint>
 #include <cstdlib>
 #include <cstring>
+#include <random>
 #include "functions.h"
 
 
 #define sqsort  qsort
+#define BUFFER_LEN  0x8000
 #define ELE_TYPE    int64_t
-#define BUFFER_LEN  0x800
 
-int cmp(const void *a, const void *b) {
-    return *(ELE_TYPE *)a > *(ELE_TYPE *)b ? 1 : -1;
+int cmp64(const void *a, const void *b) {
+    return *(int64_t *)a > *(int64_t *)b ? 1 : -1;
 }
 
-TEST(TestQuickSort1, TestQuickSort) {
-    ELE_TYPE * buffer1 = (ELE_TYPE *) malloc(BUFFER_LEN * sizeof(ELE_TYPE));
-    ELE_TYPE * buffer2 = (ELE_TYPE *) malloc(BUFFER_LEN * sizeof(ELE_TYPE));
-    ELE_TYPE * buffer3 = (ELE_TYPE *) malloc(BUFFER_LEN * sizeof(ELE_TYPE));
+TEST(TestQuickSortRandomInt64Array, TestQuickSort) {
+    auto * buffer1 = (ELE_TYPE *) malloc(BUFFER_LEN * sizeof(ELE_TYPE));
+    auto * buffer2 = (ELE_TYPE *) malloc(BUFFER_LEN * sizeof(ELE_TYPE));
+    auto * buffer3 = (ELE_TYPE *) malloc(BUFFER_LEN * sizeof(ELE_TYPE));
 
-    srand(time(0));
-    for (int i = 0; i < BUFFER_LEN; i++){ buffer1[i] = rand(); }
+    srandom(time(nullptr));
+    for (int i = 0; i < BUFFER_LEN; i++){ buffer1[i] = random(); }
     memcpy(buffer2, buffer1, BUFFER_LEN * sizeof(ELE_TYPE));
     memcpy(buffer3, buffer1, BUFFER_LEN * sizeof(ELE_TYPE));
 
-    sqsort(buffer1, BUFFER_LEN, sizeof(ELE_TYPE), cmp);
-    cqsort(buffer2, BUFFER_LEN, sizeof(ELE_TYPE), cmp);
-    _qsort(buffer3, BUFFER_LEN, sizeof(ELE_TYPE), cmp);
+    sqsort(buffer1, BUFFER_LEN, sizeof(ELE_TYPE), cmp64);
+    cqsort(buffer2, BUFFER_LEN, sizeof(ELE_TYPE), cmp64);
+    _qsort(buffer3, BUFFER_LEN, sizeof(ELE_TYPE), cmp64);
     EXPECT_EQ(memcmp(buffer2, buffer1, BUFFER_LEN), 0);
     EXPECT_EQ(memcmp(buffer3, buffer1, BUFFER_LEN), 0);
 
 
-    for (int i = 0; i < BUFFER_LEN; i++){ buffer1[i] = BUFFER_LEN - i; }
+    free(buffer1);
+    free(buffer2);
+    free(buffer3);
+}
+
+TEST(TestQuickSortConstInt64Array, TestQuickSort) {
+    auto * buffer1 = (ELE_TYPE *) malloc(BUFFER_LEN * sizeof(ELE_TYPE));
+    auto * buffer2 = (ELE_TYPE *) malloc(BUFFER_LEN * sizeof(ELE_TYPE));
+    auto * buffer3 = (ELE_TYPE *) malloc(BUFFER_LEN * sizeof(ELE_TYPE));
+
+    srandom(time(nullptr));
+    int64_t value = random();
+    for (int i = 0; i < BUFFER_LEN; i++){ buffer1[i] = value; }
     memcpy(buffer2, buffer1, BUFFER_LEN * sizeof(ELE_TYPE));
     memcpy(buffer3, buffer1, BUFFER_LEN * sizeof(ELE_TYPE));
 
-    sqsort(buffer1, BUFFER_LEN, sizeof(ELE_TYPE), cmp);
-    cqsort(buffer2, BUFFER_LEN, sizeof(ELE_TYPE), cmp);
-    _qsort(buffer3, BUFFER_LEN, sizeof(ELE_TYPE), cmp);
+    sqsort(buffer1, BUFFER_LEN, sizeof(ELE_TYPE), cmp64);
+    cqsort(buffer2, BUFFER_LEN, sizeof(ELE_TYPE), cmp64);
+    _qsort(buffer3, BUFFER_LEN, sizeof(ELE_TYPE), cmp64);
+    EXPECT_EQ(memcmp(buffer2, buffer1, BUFFER_LEN), 0);
+    EXPECT_EQ(memcmp(buffer3, buffer1, BUFFER_LEN), 0);
+
+    free(buffer1);
+    free(buffer2);
+    free(buffer3);
+}
+
+TEST(TestQuickSortReservedInt64Array, TestQuickSort) {
+    auto * buffer1 = (ELE_TYPE *) malloc(BUFFER_LEN * sizeof(ELE_TYPE));
+    auto * buffer2 = (ELE_TYPE *) malloc(BUFFER_LEN * sizeof(ELE_TYPE));
+    auto * buffer3 = (ELE_TYPE *) malloc(BUFFER_LEN * sizeof(ELE_TYPE));
+
+    srandom(time(nullptr));
+    int8_t shift = (int8_t) random() % 64;
+    for (int i = 0; i < BUFFER_LEN; i++){ buffer1[i] = BUFFER_LEN - i; buffer1[i] << shift; }
+    memcpy(buffer2, buffer1, BUFFER_LEN * sizeof(ELE_TYPE));
+    memcpy(buffer3, buffer1, BUFFER_LEN * sizeof(ELE_TYPE));
+
+    sqsort(buffer1, BUFFER_LEN, sizeof(ELE_TYPE), cmp64);
+    cqsort(buffer2, BUFFER_LEN, sizeof(ELE_TYPE), cmp64);
+    _qsort(buffer3, BUFFER_LEN, sizeof(ELE_TYPE), cmp64);
+    EXPECT_EQ(memcmp(buffer2, buffer1, BUFFER_LEN), 0);
+    EXPECT_EQ(memcmp(buffer3, buffer1, BUFFER_LEN), 0);
+
+    free(buffer1);
+    free(buffer2);
+    free(buffer3);
+}
+#undef ELE_TYPE
+
+#define ELE_TYPE    int32_t
+
+int cmp32(const void *a, const void *b) {
+    return *(int32_t *)a > *(int32_t *)b ? 1 : -1;
+}
+
+TEST(TestQuickSortRandomInt32Array, TestQuickSort) {
+    auto * buffer1 = (ELE_TYPE *) malloc(BUFFER_LEN * sizeof(ELE_TYPE));
+    auto * buffer2 = (ELE_TYPE *) malloc(BUFFER_LEN * sizeof(ELE_TYPE));
+    auto * buffer3 = (ELE_TYPE *) malloc(BUFFER_LEN * sizeof(ELE_TYPE));
+
+    srandom(time(nullptr));
+    for (int i = 0; i < BUFFER_LEN; i++){ buffer1[i] = (int32_t) random(); }
+    memcpy(buffer2, buffer1, BUFFER_LEN * sizeof(ELE_TYPE));
+    memcpy(buffer3, buffer1, BUFFER_LEN * sizeof(ELE_TYPE));
+
+    sqsort(buffer1, BUFFER_LEN, sizeof(ELE_TYPE), cmp32);
+    cqsort(buffer2, BUFFER_LEN, sizeof(ELE_TYPE), cmp32);
+    _qsort(buffer3, BUFFER_LEN, sizeof(ELE_TYPE), cmp32);
+    EXPECT_EQ(memcmp(buffer2, buffer1, BUFFER_LEN), 0);
+    EXPECT_EQ(memcmp(buffer3, buffer1, BUFFER_LEN), 0);
+
+
+    free(buffer1);
+    free(buffer2);
+    free(buffer3);
+}
+
+TEST(TestQuickSortConstInt32Array, TestQuickSort) {
+    auto * buffer1 = (ELE_TYPE *) malloc(BUFFER_LEN * sizeof(ELE_TYPE));
+    auto * buffer2 = (ELE_TYPE *) malloc(BUFFER_LEN * sizeof(ELE_TYPE));
+    auto * buffer3 = (ELE_TYPE *) malloc(BUFFER_LEN * sizeof(ELE_TYPE));
+
+    srandom(time(nullptr));
+    auto value = (int32_t) random();
+    for (int i = 0; i < BUFFER_LEN; i++){ buffer1[i] = value; }
+    memcpy(buffer2, buffer1, BUFFER_LEN * sizeof(ELE_TYPE));
+    memcpy(buffer3, buffer1, BUFFER_LEN * sizeof(ELE_TYPE));
+
+    sqsort(buffer1, BUFFER_LEN, sizeof(ELE_TYPE), cmp32);
+    cqsort(buffer2, BUFFER_LEN, sizeof(ELE_TYPE), cmp32);
+    _qsort(buffer3, BUFFER_LEN, sizeof(ELE_TYPE), cmp32);
+    EXPECT_EQ(memcmp(buffer2, buffer1, BUFFER_LEN), 0);
+    EXPECT_EQ(memcmp(buffer3, buffer1, BUFFER_LEN), 0);
+
+    free(buffer1);
+    free(buffer2);
+    free(buffer3);
+}
+
+TEST(TestQuickSortReservedInt32Array, TestQuickSort) {
+    auto * buffer1 = (ELE_TYPE *) malloc(BUFFER_LEN * sizeof(ELE_TYPE));
+    auto * buffer2 = (ELE_TYPE *) malloc(BUFFER_LEN * sizeof(ELE_TYPE));
+    auto * buffer3 = (ELE_TYPE *) malloc(BUFFER_LEN * sizeof(ELE_TYPE));
+
+    srandom(time(nullptr));
+    int8_t shift = (int8_t) random() % 32;
+    for (int i = 0; i < BUFFER_LEN; i++){ buffer1[i] = BUFFER_LEN - i; buffer1[i] << shift; }
+    memcpy(buffer2, buffer1, BUFFER_LEN * sizeof(ELE_TYPE));
+    memcpy(buffer3, buffer1, BUFFER_LEN * sizeof(ELE_TYPE));
+
+    sqsort(buffer1, BUFFER_LEN, sizeof(ELE_TYPE), cmp32);
+    cqsort(buffer2, BUFFER_LEN, sizeof(ELE_TYPE), cmp32);
+    _qsort(buffer3, BUFFER_LEN, sizeof(ELE_TYPE), cmp32);
     EXPECT_EQ(memcmp(buffer2, buffer1, BUFFER_LEN), 0);
     EXPECT_EQ(memcmp(buffer3, buffer1, BUFFER_LEN), 0);
 
